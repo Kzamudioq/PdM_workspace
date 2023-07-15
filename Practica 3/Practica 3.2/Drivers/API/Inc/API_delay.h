8c4bc9d136cd0bbfb1bdaba42e1a0371304dfeee
Kzamudioq <138271936+Kzamudioq@users.noreply.github.com>
2023-07-15 09:19:28 -0500

    Update API_delay.h

--- Practica 3/Practica 3.2/Drivers/API/Inc/API_delay.h
+++ Practica 3/Practica 3.2/Drivers/API/Inc/API_delay.h
@@ -4,17 +4,23 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-typedef uint32_t tick_t;
-typedef bool bool_t;
+typedef uint32_t tick_t; // Tipo de dato para representar el tiempo en ticks
+typedef bool bool_t; // Tipo de dato booleano
 
 typedef struct {
-   tick_t startTime;
-   tick_t duration;
-   bool_t running;
-} delay_t;
+   tick_t startTime; // Tiempo de inicio del retardo
+   tick_t duration; // Duración del retardo
+   bool_t running; // Estado de ejecución del retardo
+} delay_t; // Estructura para representar un retardo
 
+// Inicializa la estructura de retardo con la duración especificada
 void delayInit(delay_t *delay, tick_t duration);
+
+// Lee el estado del retardo
+// Devuelve true si el retardo ha terminado, o false si aún está en curso
 bool_t delayRead(delay_t *delay);
+
+// Actualiza la duración del retardo con el valor especificado
 void delayWrite(delay_t *delay, tick_t duration);
 
 #endif /* API_DELAY_H */
