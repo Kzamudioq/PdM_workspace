5923aa24cb989036d193f4cd7ff71938dc291e93
Kzamudioq <138271936+Kzamudioq@users.noreply.github.com>
2023-07-23 22:56:40 -0500

    Update API_delay.c

--- Practica 3/Practica 3.1/Drivers/API/Src/API_delay.c
+++ Practica 3/Practica 3.1/Drivers/API/Src/API_delay.c
@@ -1,29 +1,35 @@
 #include "API_delay.h"
 
+/* Inicializa la estructura de retardo no bloqueante */
 void delayInit(delay_t* delay, tick_t duration) {
    delay->duration = duration;
    delay->running = false;
 }
 
+/* Lee el retardo no bloqueante y devuelve true si ha finalizado */
 bool_t delayRead(delay_t* delay) {
    if (!delay->running) {
+      // Si el retardo no estaba en ejecución, se inicia y se registra el tiempo de inicio
       delay->startTime = HAL_GetTick();
       delay->running = true;
       return false;
    } else {
+      // Si el retardo está en ejecución, se verifica si ha finalizado
       tick_t currentTime = HAL_GetTick();
       tick_t elapsedTime = currentTime - delay->startTime;
 
       if (elapsedTime >= delay->duration) {
+         // Si ha pasado el tiempo establecido, se finaliza el retardo y devuelve true
          delay->running = false;
          return true;
       } else {
+         // Si el retardo aún no ha finalizado, devuelve false
          return false;
       }
    }
 }
 
+/* Actualiza la duración del retardo no bloqueante */
 void delayWrite(delay_t* delay, tick_t duration) {
    delay->duration = duration;
 }
-
